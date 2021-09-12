5d2bd95f6b7127368d497a483f83df58c1a9df96
Wojtek <cwiklik.wojciech@gmail.com>
2021-09-12 11:33:58 +0200

    feat: All courses

---
+++ src/components/Courses/Courses.js
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react';
+import bemCssModules from 'bem-css-modules';
+
+import Course from './subcomponents/Course';
+import { default as CoursesStyle } from './Courses.module.scss';
+import { StoreContext } from '../../store/StoreProvider';
+
+const style = bemCssModules(CoursesStyle);
+
+const Courses = () => {
+	const { courses } = useContext(StoreContext);
+
+	const coursesElements = courses.map((course) => <Course key={course.id} {...course} />);
+	return (
+		<section className={style()}>
+			<h2 className={style('title')} />
+			<ul className={style('list')}>{coursesElements}</ul>
+		</section>
+	);
+};
+
+export default Courses;
